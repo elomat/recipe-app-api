0542ee1f93bc1ed1764f2c9fe2e464d6ae9dbda6
Arzgethalm <charlesrivadelo@gmail.com>
2020-03-03 19:40:28 +0800

    added manage user api

--- app/user/tests/test_users_api.py
+++ app/user/tests/test_users_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -97,8 +98,48 @@ class PublicUserApiTest(TestCase):
 		self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
 
+	def test_retrieve_user_unauthorized(self):
+		res = self.client.post(ME_URL)
+
+		self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
+class ProvateUserApiTests(TestCase):
+
+	def setUp(self):
+		self.user = create_user(
+				email='test@test.com',
+				password='password1234',
+				name='test'
+			)
+
+		self.client = APIClient()
+		self.client.force_authenticate(user=self.user)
 
 
+	def test_retrieve_profile_success(self):
+		res = self.client.get(ME_URL)
 
+		self.assertEqual(res.status_code, status.HTTP_200_OK)
+		self.assertEqual(res.data, {
+				'name': self.user.name,
+				'email': self.user.email
+			})
+
+	def test_post_are_not_allowed(self):
+		res = self.client.post(ME_URL, {})
+
+		self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+	def test_update_user_profile(self):
+		payload = {
+			'name': 'test',
+			'password': 'newpassword1234'
+		}
+		res = self.client.patch(ME_URL, payload)
+
+		self.user.refresh_from_db()
+		self.assertEqual(self.user.name, payload['name'])
+		self.assertTrue(self.user.check_password(payload['password']))
+		self.assertEqual(res.status_code, status.HTTP_200_OK)
+		
