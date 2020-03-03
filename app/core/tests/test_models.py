3e55a4d7a751699a810a184c8da07606239a0863
Arzgethalm <charlesrivadelo@gmail.com>
2020-03-03 21:59:32 +0800

    changes

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core.models import Tag
+
+def sample_user(email='test@test.com', password='password1234'):
+	return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
@@ -35,3 +40,11 @@ class ModelTests(TestCase):
 
 		self.assertTrue(user.is_superuser)
 		self.assertTrue(user.is_staff)
+
+	def test_tag_str(self):
+		tag = Tag.objects.create(
+			user=sample_user(),
+			name='Vegan'
+			)
+
+		self.assertEqual(str(tag), tag.name)
